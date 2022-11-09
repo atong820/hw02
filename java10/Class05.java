a75e9de2e9d477952a9dc8b3b283a75b5db93033
atong820 <113488910+atong820@users.noreply.github.com>
2022-11-10 04:56:11 +0800

    Create Class05.java

---
+++ java10/Class05.java
@@ -0,0 +1,34 @@
+class Caaa
+ {
+ private int num;
+
+ public Caaa()
+ {
+ this(2); // 呼叫 Caaa(int n)建構元
+ }
+ public Caaa(int n)
+ {
+ num=n;
+
+
+ }
+ public int get()
+ {
+ return num;
+ }
+ }
+ class Cbbb extends Caaa
+{
+ public void show()
+ {
+ System.out.println("num="+get());
+ }
+ }
+ public class hw10_5a
+ {
+ public static void main(String args[])
+ {
+ Cbbb bb=new Cbbb();
+ bb.show();
+ }
+ }
